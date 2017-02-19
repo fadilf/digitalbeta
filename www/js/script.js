ff2fc77cdcc377eb7924aa43872044e3388784d8
Fadil Eledath <fadilfaizal@gmail.com>
2017-02-19 22:45:58 +0400

    server url

--- www/js/script.js
+++ www/js/script.js
@@ -1,6 +1,6 @@
 if(localStorage.getItem("serverurl") === null){
-    localStorage.setItem("serverurl", "http://localhost/server-api/api.php");
-//    localStorage.setItem("serverurl", "http://77.104.154.250/~maveri87/server-api/api.php");
+//    localStorage.setItem("serverurl", "http://localhost/server-api/api.php");
+    localStorage.setItem("serverurl", "http://77.104.154.250/~maveri87/server-api/api.php");
 }
 if(localStorage.getItem("Username") === null){
     window.location.href = "login.html";
